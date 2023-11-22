466ff58d3af2473f3f4bd0cf7fac7cff60de39ae
emilie <colleyeemilie@hotmail.com>
2023-11-22 12:13:08 +0100

    :sparkles: delete contact

--- resources/views/livewire/contacts/display-contacts.blade.php
+++ resources/views/livewire/contacts/display-contacts.blade.php
@@ -2,7 +2,14 @@
     <ul>
         @foreach($contacts as $contact)
             <li><a href="/contacts/{{ $contact->id }}"> {{ $contact->name }} {{ $contact->firstname }} </a>
-                <a href="/contacts/{{ $contact->id }}/edit">EDITER LE CONTACT</a></li>
+                <a href="/contacts/{{ $contact->id }}/edit">EDITER LE CONTACT</a>
+                <form action="/contacts/{{ $contact->id }}" method="post">
+                    @method('DELETE')
+                    @csrf
+                    <input type="hidden" name="_method" value="delete">
+                    <button type="submit">{{ __('Supprimer ce contact') }}</button>
+                </form>
+            </li>
         @endforeach
     </ul>
     {{ $contacts->links() }}
