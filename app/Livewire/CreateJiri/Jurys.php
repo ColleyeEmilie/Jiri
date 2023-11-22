12ee200af0297635758a54edc8777e2d9fd5b877
emilie <colleyeemilie@hotmail.com>
2023-11-22 15:25:29 +0100

    :sparkles: récupération ID dernier contact et dernier jiri créé

--- app/Livewire/CreateJiri/Jurys.php
+++ app/Livewire/CreateJiri/Jurys.php
@@ -2,12 +2,69 @@
 
 namespace App\Livewire\CreateJiri;
 
+use App\Models\Contact;
+use App\Models\Jiri;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Jurys extends Component
 {
-    public function render()
+    public $first;
+    public $name = '';
+    public $firstname = '';
+    public $email = '';
+    public $lastJiri;
+    public int|null $juryId;
+    public $lastJury;
+
+    #[computed]
+    public function users(){
+        return $this->name? Contact::where('name', 'like','%'.$this->name.'%')->get() : new Collection();
+    }
+
+    public function lastJiri(){
+         $this->lastJiri = Jiri::orderBy('created_at','desc')->first();
+    }
+
+    public function lastJury(){
+        $this->lastJury = Contact::orderBy('created_at','desc')->first();
+    }
+
+    public function mount($juryId = 0): void
     {
-        return view('livewire.create-jiri.jurys');
+        $this->juryId= $juryId;
+
+        if ($juryId) {
+            $jury = auth()
+                ->user()
+                ?->jiris()
+                ->findOrFail($juryId);
+            $this->name = $jury->name;
+            $this->firstname = $jury->firstname;
+            $this->email = $jury->email;
+        } else {
+            $this->name = '';
+            $this->firstname = '';
+            $this->email = '';
+        }
+    }
+
+    public function newUser(){
+        $this->lastJiri();
+        $this->lastJury();
+        dd($this->lastJiri, $this->lastJury);
+        $this->juryId = auth()
+            ->user()
+            ?->contacts()
+            ->updateOrCreate(
+                ['id' => $this->juryId],
+                [
+                    'name' => $this->name,
+                    'firstname' => $this->firstname,
+                    'email' => $this->email,
+                    'user_id' => auth()->id(),
+                ]
+            )->id;
     }
 }
--- resources/views/livewire/create-jiri/jurys.blade.php
+++ resources/views/livewire/create-jiri/jurys.blade.php
@@ -5,7 +5,37 @@
             <button @click="open=!open" x-html="open ? '-' :'+' " ></button>
         </div>
         <div x-show="open" x-cloak x-transition>
-            <p>Test</p>
+            <div class="mb-8  px-5" x-data="{contactsList : false, usersList:[]">
+                <form wire:submit="add" class="">
+                    <div class="mb-4">
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom </label>
+                        <input type="text" name="name" wire:model.live="name" list="jury" class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <datalist id="jury">
+                            @foreach($this->users as $user)
+                                <option wire:key="{{$user->id}}" value="{{$user->name}}">
+                            @endforeach
+                        </datalist>
+                        @error('username') <p class="text-red-400">{{ $message }}</p>  @enderror
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Prénom </label>
+                        <input type="firstname" name="firstname" wire:model.live="firstname" value="@foreach($this->users as $user) {{$user->firstname}} @endforeach" class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        @error('firstname') <p class="text-red-400">{{ $message }}</p>  @enderror
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email </label>
+                        <input type="email" name="email" wire:model.live="email" value="@foreach($this->users as $user) {{$user->email}} @endforeach" class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        @error('email') <p class="text-red-400">{{ $message }}</p>  @enderror
+                    </div>
+                    <button type="button" x-on:click="contactsList = true" wire:click="newUser" class="transition duration-150 bg-gray-300 hover:bg-gray-300 rounded-lg px-6 py-4 border-solid border-2 border-light-blue-500">Ajouter un contact</button>
+                </form>
+
+                <template x-if="contactsList">
+
+                </template>
+            </div>
         </div>
     </div>
 </div>
