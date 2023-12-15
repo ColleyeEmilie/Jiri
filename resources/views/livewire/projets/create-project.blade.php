b2958ed955521105c6acffc18f8c349035d2cc9c
emilie <colleyeemilie@hotmail.com>
2023-12-15 12:10:34 +0100

    :lipstick: edit style

--- resources/views/livewire/projets/create-project.blade.php
+++ resources/views/livewire/projets/create-project.blade.php
@@ -1,27 +1,27 @@
 <div>
     <form action="{{ route('projets.store') }}" method="post">
         @csrf
-        <div>
+        <div class="mb-4">
             <label class="block mb-2 text-sm font-medium text-gray-900" for="name">{{ __('Nom du projet') }} </label>
             <input wire:model.live="name" type="text" name="name" id="name" class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5">
             @error('name') <span class="error">{{ $message }}</span> @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label class="block mb-2 text-sm font-medium text-gray-900" for="link">{{ __('Lien vers le projet') }} </label>
             <input wire:model.live="link" type="text" name="link" id="link" class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5">
             @error('link') <span class="error">{{ $message }}</span> @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label class="block mb-2 text-sm font-medium text-gray-900" for="ponderation"> {{ __('Pondération') }} </label>
             <input wire:model.live="ponderation" type="number" name="ponderation" id="ponderation" class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5">
             @error('ponderation') <span class="error">{{ $message }}</span> @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label class="block mb-2 text-sm font-medium text-gray-900" for="description"> {{ __('Description') }} </label>
             <textarea id="description" name="description" rows="4" cols="50" class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"></textarea>
             @error('description') <span class="error">{{ $message }}</span> @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <button wire:click="newProject" @if($errors->any()) disabled @endif @if(empty($name || $ponderation || $link)) disabled @endif class="mb-4 mt-4 bg-transparent hover:bg-orange-400 text-orange-400 font-semibold hover:text-white py-2 px-4 border border-orange-400 hover:border-transparent rounded" type="submit">
                 {{ __('+ Ajouter le projet') }}
             </button>
--- resources/views/livewire/projets/edit-project.blade.php
+++ resources/views/livewire/projets/edit-project.blade.php
@@ -2,25 +2,25 @@
     <form action="{{ route('projets.update', $project) }}" method="post">
         @csrf
         @method('PUT')
-        <div class="mb-4">
+        <div class="mb-8">
             <label for="name" class="block mb-2 text-sm font-medium text-gray-900"> {{ __('Nom') }} </label>
             <input type="text" id="name" name="name" class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 " value="{{ $project->name }}" required>
         </div>
 
-        <div class="mb-4">
+        <div class="mb-8">
             <label for="link" class="block mb-2 text-sm font-medium text-gray-900"> {{ __('Lien') }} </label>
             <input type="text" id="link" name="link" class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 " value="{{ $project->link }}" >
         </div>
 
-        <div class="mb-4">
+        <div class="mb-8">
             <label for="ponderation" class="block mb-2 text-sm font-medium text-gray-900"> {{ __('Pondération') }} </label>
             <input type="number" id="ponderation" name="ponderation" class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 " value="{{ $project->ponderation }}">
         </div>
-        <div class="mb-4">
+        <div class="mb-8">
             <label for="description" class="block mb-2 text-sm font-medium text-gray-900"> {{ __('Description') }} </label>
             <textarea id="description" name="description" rows="4" cols="50" class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 " value="{{ $project->description }}" required></textarea>
         </div>
-        <div class="mb-4">
+        <div class="mb-8">
             <button class="bg-transparent hover:bg-orange-400 text-orange-400 font-semibold hover:text-white py-2 px-4 border border-orange-400 hover:border-transparent rounded" type="submit">
                 {{ __(('Modifier le projet')) }}
             </button>
