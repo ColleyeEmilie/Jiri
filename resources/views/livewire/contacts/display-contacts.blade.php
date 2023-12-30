ab90071a293b1f9f57da628f17ec67c9a11cbf05
emilie <colleyeemilie@hotmail.com>
2023-12-30 02:25:15 +0100

    :lipstick: add css page contact

--- resources/views/livewire/contacts/display-contacts.blade.php
+++ resources/views/livewire/contacts/display-contacts.blade.php
@@ -1,7 +1,8 @@
 <div>
     @if(session('success'))
-        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative max-w-7xl mx-auto sm:px-6 lg:px-8"
-             role="alert">
+        <div
+            class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative max-w-7xl mx-auto sm:px-6 lg:px-8"
+            role="alert">
                 <span>
                     {{ session('success') }}
                 </span>
@@ -9,32 +10,35 @@
         </div>
     @endif
     <div class="bg-white relative content-position">
-        <ul class="flex pl-6 ml-6 mb-6">
-            <li class="self-center ml-10 w-48 font-bold">NOM</li>
-            <li class="self-center ml-4 w-48 font-bold">PRENOM</li>
-            <li class="self-center ml-4 w-72 font-bold">EMAIL</li>
-        </ul>
-        <ul class="top-16">
+        <table>
+            <thead>
+            <tr class="flex pl-6 ml-6 mb-6">
+                <th class="w-48 font-bold">NOM</th>
+                <th class="w-48 font-bold">PRENOM</th>
+                <th class="w-72 font-bold" >EMAIL</th>
+            </tr>
+            </thead>
+
+            <tbody>
             @foreach($contacts as $contact)
-                <li class="mb-6 flex">
-                    <a class="flex" href="/contacts/{{ $contact->id }}">
-                        <div class="relative w-12 h-12">
-                            <img src="{{ asset($contact->image ?? 'uploads/default.jpeg') }}" alt="avatar" class=" w-12 h-12 rounded-full border border-gray-100 shadow-sm">
-                        </div>
-                        <p class="self-center ml-10 w-48">{{ $contact->name }} </p>
-                        <p class="self-center ml-4 w-48">{{ $contact->firstname }} </p>
-                        <p class="self-center ml-4 w-72">{{ $contact->email }} </p>
-                    </a>
-                    <a class="self-center ml-4" href="/contacts/{{ $contact->id }}/edit"><img class="w-6 h-6" src="{{asset('icons/crayon.svg')}}" alt="icon de modification d'utilisateur"></a>
-                    <form  class="self-center ml-4 h-6" action="/contacts/{{ $contact->id }}" method="post">
-                        @method('DELETE')
-                        @csrf
-                        <input type="hidden" name="_method" value="delete">
-                        <button type="submit"><img class="w-6 h-6" src="{{asset('icons/delete.svg')}}" alt="icon de suppression d'utilisateur"></button>
-                    </form>
-                </li>
+                <tr class="mb-6 flex">
+                    <td> <a class="flex" href="/contacts/{{ $contact->id }}"><img src="{{ asset($contact->image ?? 'uploads/default.jpeg') }}" alt="avatar"
+                             class=" w-12 h-12 rounded-full border border-gray-100 shadow-sm"></a></td>
+                    <td class="self-center ml-10 w-48"> <a class="flex" href="/contacts/{{ $contact->id }}">{{ $contact->name }}</a></td>
+                    <td class="self-center ml-10 w-48"> <a class="flex" href="/contacts/{{ $contact->id }}">{{ $contact->firstname }}</a></td>
+                    <td class="self-center ml-10 w-72"> <a class="flex" href="/contacts/{{ $contact->id }}">{{ $contact->email }}</a></td>
+                    <td><a class="self-center ml-4" href="/contacts/{{ $contact->id }}/edit"><img class="w-6 h-6" src="{{asset('icons/crayon.svg')}}" alt="icon de modification d'utilisateur"></a></td>
+                    <td><form class="self-center ml-4 h-6" action="/contacts/{{ $contact->id }}" method="post">
+                            @method('DELETE')
+                            @csrf
+                            <input type="hidden" name="_method" value="delete">
+                            <button type="submit"><img class="w-6 h-6" src="{{asset('icons/delete.svg')}}"
+                                                       alt="icon de suppression d'utilisateur"></button>
+                        </form></td>
+                </tr>
             @endforeach
-        </ul>
+            </tbody>
+        </table>
     </div>
     {{ $contacts->links() }}
 </div>
