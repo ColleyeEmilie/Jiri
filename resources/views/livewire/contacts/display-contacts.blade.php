f63ae620aaf60eab82b521946af1f3e7d6d95fb4
emilie <colleyeemilie@hotmail.com>
2024-01-02 16:24:22 +0100

    :lipstick: change style projects/contacts

--- resources/views/livewire/contacts/display-contacts.blade.php
+++ resources/views/livewire/contacts/display-contacts.blade.php
@@ -12,23 +12,23 @@
     <div class="bg-white relative content-position">
         <table>
             <thead>
-            <tr class="flex pl-6 ml-6 mb-6">
-                <th class="w-48 font-bold">NOM</th>
-                <th class="w-48 font-bold">PRENOM</th>
-                <th class="w-72 font-bold" >EMAIL</th>
+            <tr class="flex mb-6">
+                <th class="w-48 font-bold">{{ __('NOM') }}</th>
+                <th class="w-48 font-bold">{{ __('PRENOM') }}</th>
+                <th class="w-72 font-bold" >{{ __('EMAIL') }}L</th>
             </tr>
             </thead>
 
             <tbody>
             @foreach($contacts as $contact)
                 <tr class="mb-6 flex">
-                    <td> <a class="flex" href="/contacts/{{ $contact->id }}"><img src="{{ asset($contact->image ?? 'uploads/default.jpeg') }}" alt="avatar"
+                    <td> <a class="flex w-12 h-12 mr-10" href="/contacts/{{ $contact->id }}" ><img src="{{ asset($contact->image ?? 'uploads/default.jpeg') }}" alt="avatar"
                              class=" w-12 h-12 rounded-full border border-gray-100 shadow-sm"></a></td>
-                    <td class="self-center ml-10 w-48"> <a class="flex" href="/contacts/{{ $contact->id }}">{{ $contact->name }}</a></td>
+                    <td class="self-center w-48"> <a class="flex" href="/contacts/{{ $contact->id }}">{{ $contact->name }}</a></td>
                     <td class="self-center ml-10 w-48"> <a class="flex" href="/contacts/{{ $contact->id }}">{{ $contact->firstname }}</a></td>
                     <td class="self-center ml-10 w-72"> <a class="flex" href="/contacts/{{ $contact->id }}">{{ $contact->email }}</a></td>
-                    <td><a class="self-center ml-4" href="/contacts/{{ $contact->id }}/edit"><img class="w-6 h-6" src="{{asset('icons/crayon.svg')}}" alt="icon de modification d'utilisateur"></a></td>
-                    <td><form class="self-center ml-4 h-6" action="/contacts/{{ $contact->id }}" method="post">
+                    <td class="flex"><a class="self-center ml-4" href="/contacts/{{ $contact->id }}/edit"><img class="w-6 h-6" src="{{asset('icons/crayon.svg')}}" alt="icon de modification d'utilisateur"></a></td>
+                    <td class="flex"><form class="self-center ml-4 h-6" action="/contacts/{{ $contact->id }}" method="post">
                             @method('DELETE')
                             @csrf
                             <input type="hidden" name="_method" value="delete">
--- resources/views/livewire/projets/display-project.blade.php
+++ resources/views/livewire/projets/display-project.blade.php
@@ -1,28 +1,41 @@
 <div class="bg-white relative content-position">
     @if(session('success'))
-        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative max-w-7xl mx-auto sm:px-6 lg:px-8"
-             role="alert">
+        <div
+            class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative max-w-7xl mx-auto sm:px-6 lg:px-8"
+            role="alert">
                 <span>
                     {{ session('success') }}
                 </span>
         </div>
-            @endif
-            <ul>
+    @endif
+
+            <table>
+                <thead>
+                <tr class="flex mb-6">
+                    <th class="w-48 flex font-bold">{{ __('NOM DU PROJET') }}</th>
+                    <th class="w-48 flex ml-10 font-bold">{{ __('LIEN DES CONSIGNES') }}</th>
+                    <th class="w-72 flex ml-10 font-bold" >{{ __('DESCRIPTION') }}</th>
+                </tr>
+                </thead>
+
+                <tbody>
                 @foreach($projects as $project)
-                    <li class="mb-4 flex">
-                        <a href="/projets/{{ $project->id }}"> {{ $project->name }} </a>
-                        <a class="ml-2 self-center" href="/projets/{{ $project->id }}/edit"><img class="w-6 h-6"
-                                                                                                 src="{{asset('icons/crayon.svg')}}"
-                                                                                                 alt="icon de modification d'utilisateur"></a>
-                        <form class="ml-2 self-center" action="/projets/{{ $project->id }}" method="post">
-                            @method('DELETE')
-                            @csrf
-                            <input type="hidden" name="_method" value="delete">
-                            <button type="submit"><img class="w-6 h-6" src="{{asset('icons/delete.svg')}}"
-                                                       alt="icon de suppression d'utilisateur"></button>
-                        </form>
-                    </li>
+                    <tr class="mb-6 flex">
+                        <td class="self-center w-48"> <a class="flex" href="/projets/{{ $project->id }}">{{ $project->name }}</a></td>
+                        <td class="self-center ml-10 w-48"> <a class="flex" href="/projets/{{ $project->id }}">{{ $project->link }}</a></td>
+                        <td class="self-center ml-10 w-72"> <a class="flex" href="/projets/{{ $project->id }}">{{ $project->description }}</a></td>
+                        <td class="flex"><a class="self-center ml-4" href="/projets/{{ $project->id }}/edit"><img class="w-6 h-6" src="{{asset('icons/crayon.svg')}}" alt="icon de modification d'utilisateur"></a></td>
+                        <td class="flex"><form class="self-center ml-4 h-6" action="/projets/{{ $project->id }}" method="post">
+                                @method('DELETE')
+                                @csrf
+                                <input type="hidden" name="_method" value="delete">
+                                <button type="submit"><img class="w-6 h-6" src="{{asset('icons/delete.svg')}}"
+                                                           alt="icon de suppression d'utilisateur"></button>
+                            </form></td>
+                    </tr>
                 @endforeach
-                {{ $projects->links() }}
-    </ul>
+                </tbody>
+            </table>
+
+    {{ $projects->links() }}
 </div>
